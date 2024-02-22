c6736c68a1779c01245e6e6ccb5c2dc21103fa1c
deaxparadox <nitish136900kushwaha@gmail.com>
2024-02-23 03:23:01 +0530

    lts api server, websocket working

--- lts/backend/main.py
+++ lts/backend/main.py
@@ -1,10 +1,11 @@
 from typing import Union
 
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.middleware.cors import CORSMiddleware
 
 import backend.settings.local as settings
 from backend.models import HelloModel
+from backend.ws import manager
 
 app = FastAPI()
 
@@ -20,11 +21,24 @@ app.add_middleware(
 
 
 @app.get("/", response_model=HelloModel)
-def read_root():
+async def read_root():
 
     return {"message": settings.transcriber("hello everyone")[0]["translation_text"]}
 
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
+async def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.websocket("/ws/{client_id}")
+async def websocket_connect(websocket: WebSocket, client_id: int):
+    await manager.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await manager.send_personal_message(f"You wrote: {data}", websocket)
+            # await manager.broadcast(f"Client #{client_id} says: {data}")
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        # await manager.broadcast(f"Client #{client_id} left the chat")
--- lts/backend/settings/local.py
+++ lts/backend/settings/local.py
@@ -1,2 +1,4 @@
 from .base import *
 from backend.npl.load import transcriber
+
+ALLOWED_HOSTS = ALLOWED_HOSTS + ["https://piehost.com/websocket-tester"]
